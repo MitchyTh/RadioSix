b12fefe52506bfd501ab3ed0c6e30b3b7514be76
user <user@FIH-COM24a205.fsu.edu>
2025-10-18 17:18:23 -0400

    Adds enemy tracking and chasing

---
+++ Assets/agentMove.cs
@@ -0,0 +1,31 @@
+using System.Timers;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentFollowPlayer : MonoBehaviour
+{
+    public Transform player;  // Drag your player GameObject here
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (agent != null && agent.isOnNavMesh && player != null)
+        {
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(player.position, out hit, 2f, NavMesh.AllAreas))
+            {
+                agent.SetDestination(player.position);
+            }
+            else
+            {
+                Debug.Log("Player not being tracked on navmesh");
+            }
+        }
+    }
+}
