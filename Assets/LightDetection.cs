4576d8c8bb736833c4c99927b9d2c7cda7db7e99
Ares <agregory3@fsu.edu>
2025-10-17 22:20:22 -0400

    Kinda light detection?

--- Assets/LightDetection.cs
+++ Assets/LightDetection.cs
@@ -1,10 +1,15 @@
- using UnityEngine;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class LightDetection : MonoBehaviour
 {
     [Range(0f, 1f)] public float LightLevel = 0f;
+
     private Vector3 position;
     private bool flashlightOn = false;
+
+    public Light[] lights;
 
     private void Start()
     {
@@ -13,6 +18,25 @@ public class LightDetection : MonoBehaviour
 
     private void Update()
     {
-        // AAA
+        foreach (var light in lights)
+        {
+            if (GetLightIntensity(light))
+            {
+                Debug.Log("Not hit by light.");
+            }
+            else
+            {
+                Debug.Log("Hit by light.");
+            }
+        }
+    }
+    
+    bool GetLightIntensity(Light light)
+    {
+        RaycastHit hit;
+        Vector3 lightPosition = light.transform.position;
+        Physics.Raycast(transform.position, lightPosition, out hit, Mathf.Infinity);
+        Debug.DrawRay(transform.position, lightPosition * hit.distance, Color.yellow);
+        return (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity));
     }
 }
