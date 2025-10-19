84e3d4bf0ea76154328e1d6c6fe3588f5b52b63a
Ares <agregory3@fsu.edu>
2025-10-19 16:02:54 -0400

    adds in the pulse monitor to its own scene

--- Assets/PulseScroller.cs
+++ Assets/PulseScroller.cs
@@ -6,10 +6,11 @@ public class ECGScroller : MonoBehaviour
     [Header("References")]
     public RawImage ecgImage;
 
-    [Header("Settings")]
-    [Range(0f, 1f)]
-    public float dangerLevel = 0f;
 
+    [Header("Settings")]
+    //[Range(0f, 1f)]
+    //public float dangerLevel = 0f;
+    public GameObject playerDangerLevel;
     public float minScrollSpeed = 0.1f;
     public float maxScrollSpeed = 1.0f;
 
@@ -20,8 +21,11 @@ void Update()
 {
     if (ecgImage == null) return;
 
-    float scrollSpeed = Mathf.Lerp(minScrollSpeed, maxScrollSpeed, dangerLevel);
+    float stressLevel = playerDangerLevel.GetComponent<LightDetection>().stressLevel;
+    Debug.Log("test");
 
+    float scrollSpeed = Mathf.Lerp(minScrollSpeed, maxScrollSpeed, stressLevel);
+    
     previousOffset = currentOffset;
     currentOffset += scrollSpeed * Time.deltaTime;
     currentOffset %= 1f;
