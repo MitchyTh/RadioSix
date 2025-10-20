9e3d27f791e3f7b139ba4038f3adbdb122b6d068
jake0822 <jakeyounan16@gmail.com>
2025-10-20 09:37:40 -0400

    adds OneNDone option for scare zones

--- Assets/AudioQueueTrigger.cs
+++ Assets/AudioQueueTrigger.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class AudioQueueTrigger : MonoBehaviour
 {
     public AudioSource audioSource;
+
+    public bool oneNDone = false;
+
+    private bool flag = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,13 +16,18 @@ public class AudioQueueTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(flag)
+            return;
         Debug.Log("Enters area");
         if (other.CompareTag("Player"))
         {
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
+                if (oneNDone)
+                    flag = true;
             }
         }
+        
     }
 }
